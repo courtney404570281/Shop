ece401e986271e78283115124702c889a4341b6d
CourtneyLiang <404570281@gapp.fju.edu.tw>
2019-06-06 20:56:48 +0800

    Add RecyclerView

--- app/src/main/java/com/courtney/MainActivity.kt
+++ app/src/main/java/com/courtney/MainActivity.kt
@@ -4,13 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -19,6 +21,8 @@ import com.google.firebase.database.ValueEventListener
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.row_function.*
+import kotlinx.android.synthetic.main.row_function.view.*
 import org.jetbrains.anko.sdk27.coroutines.onItemClick
 import org.jetbrains.anko.sdk27.coroutines.onItemSelectedListener
 
@@ -30,6 +34,12 @@ class MainActivity : AppCompatActivity() {
     val TAG = MainActivity::class.java.simpleName
     val auth = FirebaseAuth.getInstance()
     val signup = false
+    val functions = listOf<String>("Camera",
+        "Invite Friends",
+        "Parking",
+        "Download coupons",
+        "News",
+        "Maps")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         // Spinner
         val colors = arrayOf("Red", "Blue", "Green")
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, colors)
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, colors)
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
         spinner.adapter = adapter
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -63,6 +73,39 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "onItemSelected: ${colors[position]}")
             }
         }
+
+        // RecyclerView
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.setHasFixedSize(true)
+        recycler.adapter = FunctionAdapter()
+
+    }
+
+    inner class FunctionAdapter : RecyclerView.Adapter<FunctionHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.row_function, parent, false)
+            return FunctionHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return functions.size
+        }
+
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.functionText.text = functions[position]
+            holder.itemView.setOnClickListener {
+                functionClicked(holder, position)
+            }
+        }
+    }
+
+    private fun functionClicked(holder: FunctionHolder, position: Int) {
+//        Log.d(TAG, "functionClicked: $position")
+    }
+
+
+    class FunctionHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var functionText : TextView = itemView.txt_functions
     }
 
     private fun authChanged(auth: FirebaseAuth) {
